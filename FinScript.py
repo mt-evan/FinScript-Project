01d8f7e911f3c8a6f840f6a725178fad952287f9
mt-evan <10evantardiff@gmail.com>
2024-12-03 00:28:33 -0800

    did a shortcut to assign variables to True/False

--- FinScript.py
+++ FinScript.py
@@ -8,6 +8,7 @@ finscript_mm = metamodel_from_file('FinScript.tx')
 class FinScriptInterpreter:
     def __init__(self):
         self.state = {}
+        self.keywords = ['let', 'print', 'calculate', 'if', 'then', 'endif', 'True', 'False']
 
     def interpret(self, model):
         for s in model.statements:
@@ -23,13 +24,20 @@ class FinScriptInterpreter:
 
             # Declaration
             elif s.__class__.__name__ == "DeclarationValue":
+                print("declaring with a literal value")
                 self.state[s.name] = s.value
             elif s.__class__.__name__ == "DeclarationVar":
                 if s.value in self.state: # Check if RHS variable is in state
                     self.state[s.name] = self.state[s.value]
                 else:
-                    print(f"Variable not found: {s.value}")
-                    break
+                    # Check if it is "True" or "False"
+                    if s.value == "True":
+                            self.state[s.name] = True
+                    elif s.value == "False":
+                            self.state[s.name] = False
+                    else:
+                        print(f"Variable not found: {s.value}")
+                        break
 
 # Test Program
 finscript_model = finscript_mm.model_from_file('sandbox.fin')
