e6b330cb8aa432a97b0697092fdeb7edcdfae005
mt-evan <10evantardiff@gmail.com>
2024-12-04 16:17:34 -0800

    passed tests. Cntinue and break are implemented

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,23 @@
-let a = 5
-let b = 10
-let c = 3
-let result = (a < b && c > a) || (b == 10)
-console.log(result)
+for (let i = 1; i <= 3; i++) {
+    console.log("Outer loop");
+    console.log(i);
+    let j = 1;
+    while (j <= 3) {
+        if (j == 2) {
+            console.log("Skipping inner loop");
+            console.log(j);
+            j = j + 1;
+            continue;
+        }
+        if (i == 3 && j == 3) {
+            console.log("Breaking all loops at");
+            console.log(i);
+            console.log(j);
+            break;
+        }
+        console.log("Inner loop");
+        console.log(j);
+        console.log("RIGHT HEREEEEE");
+        j = j + 1;
+    }
+}
